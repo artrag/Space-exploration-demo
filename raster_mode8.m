c2989ea4b55c386fb74a464e183b382366b8ed4a
artrag <artrag@users.noreply.github.com>
2017-10-21 15:50:06 +0200

    Exporation of Mars surface - msx 2 demo

---
+++ raster_mode8.m
@@ -0,0 +1,57 @@
+clear
+close all
+
+red   = [0,  36,  73, 109, 146, 182, 219, 255];
+green = [0,  36,  73, 109, 146, 182, 219, 255];
+blue  = [0,  73, 146, 255];
+
+i=1;
+for g=0:7
+    for r=0:7
+        for b=0:3
+            pal(i,:) = ([red(r+1) green(g+1) blue(b+1)]/255);
+            i=i+1;
+        end
+    end
+end
+
+fid = fopen('scr8.act','w');
+for i=1:256
+    fwrite(fid,uint8(pal(i,:)*255),'uint8');
+end
+fclose(fid);
+
+
+[A,MAP]= imread('plasma.bmp');
+A = [A(400:400+127,400:400+255,:);A(400+127:-1:400,400:400+255,:)];
+figure;
+image([A;A]);
+if any(size(MAP)==0)
+    [A,MAP] = rgb2ind(A,256);
+end
+[ A MAP] = imapprox(A,MAP,pal, 'nodither');
+fid = fopen('plasma.bin','wb');
+for i=1:min([256, size(A,1)]);
+    fwrite(fid,A(i,:),'uint8');
+end
+fclose(fid);
+
+%---
+[B,MAP]= imread('nebula256.bmp');
+
+figure;
+image(B);
+colormap(MAP);
+if any(size(MAP)==0)
+    [B,MAP] = rgb2ind(B,256);
+end
+[B MAP] = imapprox(B,MAP,pal, 'nodither');
+fid = fopen('galaxy.bin','wb');
+for i=1:min([256, size(B,1)]);
+    fwrite(fid,B(i,:),'uint8');
+end
+fclose(fid);
+
+
+!compila_ras.bat
+
